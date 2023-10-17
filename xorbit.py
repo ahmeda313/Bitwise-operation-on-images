2ba666d2650221228520584d10bafb577dba3598
ahmeda313 <syedahmeda313@gmail.com>
2023-10-17 18:52:39 +0530

    new project

---
+++ xorbit.py
@@ -0,0 +1,27 @@
+# Python programe to illustrate 
+# arithmetic operation of 
+# bitwise XOR of two images 
+	
+# organizing imports 
+import cv2 
+import numpy as np 
+	
+# path to input images are specified and 
+# images are loaded with imread command 
+image1 = cv2.imread('input3.png') 
+image2 = cv2.imread('input4.png')
+a=image2.shape
+print(a)
+
+# cv2.bitwise_xor is applied over the 
+# image inputs with applied parameters 
+dest_xor = cv2.bitwise_xor(image1, image2, mask = None) 
+
+# the window showing output image 
+# with the Bitwise XOR operation 
+# on the input images 
+cv2.imshow('Bitwise XOR', dest_xor) 
+
+# De-allocate any associated memory usage 
+if cv2.waitKey(0) & 0xff == 27: 
+	cv2.destroyAllWindows() 
